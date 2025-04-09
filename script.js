39dc04057a84587684ab13659b0684fe43405ccd
Carson-Owen <owenca@vcu.edu>
2025-04-09 19:54:19 -0400

    Create speech recognition; Implement onSpeak(event)

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ const msgEl = document.getElementById('msg');
 function getRandomNumber() {
   return Math.floor(Math.random() * 100) + 1;
 }
+
+const randomNum = getRandomNumber();
+console.log('Number:', randomNum);
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+let recognition = new window.SpeechRecognition();
+
+// Start recognition and game
+recognition.start();
+
+// Capture user speak
+function onSpeak(event) {
+    const msg = event.results[0][0].transcript;  // You can log the event to view the structure of the data
+    console.log(msg);
+  }
+  
+  // Speak result
+  recognition.addEventListener('result', onSpeak);
