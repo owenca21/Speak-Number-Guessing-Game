5ee7ceaf72efa8ffd6465ad0f86acac94845969b
Carson-Owen <owenca@vcu.edu>
2025-04-09 20:06:52 -0400

    Implement number guessing logic

--- script.js
+++ script.js
@@ -21,16 +21,70 @@ function onSpeak(event) {
     console.log(msg);
   }
   
-  // Speak result
-  recognition.addEventListener('result', onSpeak);
+// Speak result
+recognition.addEventListener('result', onSpeak);
 
-  // Write what user speaks
-function writeMessage(msg) {
-    const div = document.createElement('div');
-    div.textContent = 'You said: ';
-    const span = document.createElement('span');
-    span.classList.add('box');
-    span.textContent = msg;
-  
-    msgEl.append(div, span);
-  }
+// Check msg against the secret number
+function checkNumber(msg) {
+    let num = Number(msg);
+
+    const wordToNumber = {
+        one: 1,
+        won: 1,
+        two: 2,
+        to: 2,
+        too: 2,
+        three: 3,
+        four: 4,
+        for: 4,
+        five: 5,
+        six: 6,
+        seven: 7,
+        eight: 8,
+        ate: 8,
+        nine: 9,
+        ten: 10,
+    };
+
+    if (wordToNumber[msg]) {
+        console.log(`adjusting ${msg} to ${wordToNumber[msg]}`);
+        num = wordToNumber[msg];  // Reassign num after conversion
+    }
+
+    // Check if the spoken content is a valid number
+    if (Number.isNaN(num)) {
+        const div = document.createElement('div');
+        div.textContent = 'That is not a valid number';
+        msgEl.append(div);
+        return;
+    }
+
+    // Check if it's in range
+    if (num < 1 || num > 100) {
+        const div = document.createElement('div');
+        div.textContent = 'Number must be between 1 and 100';
+        msgEl.append(div);
+        return;
+    }
+
+    // Check the number and provide feedback
+    if (num === randomNum) {
+        const h2 = document.createElement('h2');
+        h2.textContent = `Congrats! You have guessed the number! It was ${num}`;
+        const button = document.createElement('button');
+        button.classList.add('play-again');
+        button.id = 'play-again';
+        button.textContent = 'Play Again';
+        button.addEventListener('click', () => window.location.reload());
+
+        msgEl.append(h2, button);
+    } else if (num > randomNum) {
+        const div = document.createElement('div');
+        div.textContent = 'GO LOWER';
+        msgEl.append(div);
+    } else {
+        const div = document.createElement('div');
+        div.textContent = 'GO HIGHER';
+        msgEl.append(div);
+    }
+}
